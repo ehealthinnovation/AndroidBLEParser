1ed52e4214f467ffa784e1b804b07f2f126a7dbc
Kuo-Cheng Tong <kuochengtong@PHIT-MBP41.local>
2018-10-22 18:01:47 -0400

    Delete unused files

--- parser/src/main/java/org/ehealthinnovation/android/bluetooth/parser/DataBufferInterface.kt
+++
@@ -1,7 +0,0 @@
-package org.ehealthinnovation.android.bluetooth.parser
-
-interface DataBufferInterface {
-    fun getNextInt(formatType: BLEDataFormatType):Int
-    fun getNextFloat(formatType: BLEDataFormatType):Float
-    fun getNextDataBytes(length: Int):ByteArray
-}
