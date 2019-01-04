99ef054bb2132a6120799cd968db6cf0ac2dc77e
hqiu <hqiu@ehealthinnovation.org>
2019-01-04 15:05:17 -0500

    Revert "add documentation (#143)" (#144)
    This reverts commit 39164d2a8c176497d1ccce1440d5967a5bde7540.

--- parser/src/main/java/org/ehealthinnovation/android/bluetooth/idd/IddHistoryDataParser.kt
+++ parser/src/main/java/org/ehealthinnovation/android/bluetooth/idd/IddHistoryDataParser.kt
@@ -5,55 +5,7 @@ import org.ehealthinnovation.android.bluetooth.idd.historydata.*
 import org.ehealthinnovation.android.bluetooth.parser.*
 
 /**
- * Parser to parse the response from a Idd Command Control Point characteristic. To use this parser,
- * call [canParse] first to make sure the parser can handle the input packet. Call [parse] afterwards
- * to get a instance of [HistoryEvent].
- *
- * The data type of [HistoryEvent.eventData] is determined by [HistoryEvent.eventInfo], specifically
- * the [EventInfo.type] field. User can cast the [HistoryEvent.eventData] to a data type according to
- * the following mapping.
- *
- * | Event Type                                     | Data Type           |
- * |:------------------------------------:          |:-------------------:|
- * |[EventType.REFERENCE_TIME]                      |[ReferenceTime]      |
- * |[EventType.REFERENCE_TIME_BASE_OFFSET]          |[ReferenceTimeBaseOffsetData]|
- * |[EventType.BOLUS_CALCULATED_PART_1_OF_2]        |[BolusCalculatedData]|
- * |[EventType.BOLUS_CALCULATED_PART_2_OF_2]        |[BolusCalculatedData]|
- * |[EventType.BOLUS_PROGRAMMED_PART_1_OF_2]        |[Bolus]|
- * |[EventType.BOLUS_PROGRAMMED_PART_2_OF_2]        |[BolusProgrammedConfiguration]|
- * |[EventType.BOLUS_DELIVERED_PART_1_OF_2]         |[Bolus]|
- * |[EventType.BOLUS_DELIVERED_PART_2_OF_2]         |[BolusDeliveredStatus]|
- * |[EventType.DELIVERED_BASAL_RATE_CHANGED]        |[BasalRateChanged]|
- * |[EventType.TBR_ADJUSTMENT_STARTED]              |[TbrAdjustmentStart]|
- * |[EventType.TBR_ADJUSTMENT_ENDED]                |[TbrAdjustmentEnded]|
- * |[EventType.TBR_ADJUSTMENT_CHANGED]              |[TbrAdjustmentChanged]|
- * |[EventType.PROFILE_TEMPLATE_ACTIVATED]          |[ProfileTemplateActivated]|
- * |[EventType.BASAL_RATE_PROFILE_TEMPLATE_TIME_BLOCK_CHANGED]| [SingleValueTimeBlockChangeData]|
- * |[EventType.TOTAL_DAILY_INSULIN_DELIVERY]        |[TotalDailyInsulinDelivery]|
- * |[EventType.THERAPY_CONTROL_STATE_CHANGED]       |[TherapyControlStateChangedData]|
- * |[EventType.OPERATIONAL_STATE_CHANGED]           |[OperationalStateChangedData]|
- * |[EventType.RESERVOIR_REMAINING_AMOUNT_CHANGED]  |[ReservoirRemainingAmount]|
- * |[EventType.ANNUNCIATION_STATUS_CHANGED_PART_1_OF_2]|[AnnunciationStatusChanged]|
- * |[EventType.ANNUNCIATION_STATUS_CHANGED_PART_2_OF_2]|[AnnunciationStatusChangeAdditionalInfo]|
- * |[EventType.ISF_PROFILE_TEMPLATE_TIME_BLOCK_CHANGED]|[SingleValueTimeBlockChangeData]|
- * |[EventType.I2CHO_RATIO_PROFILE_TEMPLATE_TIME_BLOCK_CHANGED]|[SingleValueTimeBlockChangeData]|
- * |[EventType.TARGET_GLUCOSE_RANGE_PROFILE_TEMPLATE_TIME_BLOCK_CHANGED]|[SingleValueTimeBlockChangeData]|
- * |[EventType.PRIMING_STARTED]                     |[PrimingStarted]|
- * |[EventType.PRIMING_DONE]                        |[PrimingDone]|
- * |[EventType.DATA_CORRUPTION]                     |[EmptyEventData]|
- * |[EventType.POINTER_EVENT]                       |[EmptyEventData]|
- * |[EventType.BOLUS_TEMPLATE_CHANGED_PART_1_OF_2]  |[BolusTemplateChangedBolusPart]|
- * |[EventType.BOLUS_TEMPLATE_CHANGED_PART_2_OF_2]  |[BolusTemplateChangedBolusDelay]|
- * |[EventType.TBR_TEMPLATE_CHANGED]                |[TbrTemplateChanged]|
- * |[EventType.MAX_BOLUS_AMOUNT_CHANGED]            |[MaxBolusAmountChanged]|
- *
- *  Example for mapping the history event data to the correct data type,
- *
- *  ` val historyEvent = IddHistoryDataParser().parse(packet)
- *    if( historyEvent.eventInfo.type == EventType.PRIMING_DONE){
- *          someFunctionRequiresPrimingDoneData((historyEvent.eventData as PrimingDone))
- *    }`
- *
+ * Parser to parse the response from a Idd Command Control Point
  */
 class IddHistoryDataParser : CharacteristicParser<HistoryEvent<out Any>> {
     override fun canParse(packet: CharacteristicPacket): Boolean {
